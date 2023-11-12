8d9ab1d6001e9dc71614d4666e21077049e9afae
DiegoDefSan <defsan35@gmail.com>
2023-11-11 23:40:08 -0300

    Add color constants to utils

---
+++ lib/utils/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const Map<String, Color> colors = {
+  "pink-navbar": Color(0xFFEF798A),
+  "pink-background": Color(0xFFE5C3D1),
+  "white": Color(0xFFFFFFFF),
+  "purple-button": Color(0xFFA48AB4),
+  "purple-text": Color(0xFF8E779B),
+  "pink-container": Color(0xFFFCF1F1),
+  "pink-letter": Color(0xFFEC6276),
+  "blue-container": Color(0xFFA2A6D1),
+  "very-poor-quality": Color(0xFFFF8C5A),
+  "poor-quality": Color(0xFFFF8C5A),
+  "moderate-quality": Color(0xFFFF8C5A),
+  "fair-quality": Color(0xFFBADE50),
+  "good-quality": Color(0xFF87A44A),
+};
