edfb38879e05f905e477a0237f307b2bba3cbe15
DiegoDefSan <defsan35@gmail.com>
2023-11-13 20:28:26 -0300

    Add CityApiService class for fetching city data

---
+++ lib/services/city_api_service.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:previsao_do_tempo/models/city.dart';
+import 'package:previsao_do_tempo/models/weather.dart';
+import 'package:previsao_do_tempo/services/weather_api_service.dart';
+import 'package:previsao_do_tempo/utils/constants.dart';
+
+class CityApiService {
+  final String baseUrl = "http://api.openweathermap.org/geo/1.0/";
+
+  Future<City> getCity(String cityName) async {
+    final responseCity = await http
+        .get(Uri.parse("${baseUrl}direct?q=$cityName&limit=1&appid=$apiKey"));
+
+    if (responseCity.statusCode != 200) {
+      throw Exception("Failed to load city");
+    }
+
+    final jsonResponse = jsonDecode(responseCity.body);
+
+    final Map<String, dynamic> cityData = jsonResponse[0];
+
+    City city = City(
+      name: cityData["name"],
+      latitude: cityData["lat"],
+      longitude: cityData["lon"],
+      countryCode: cityData["country"],
+    );
+
+    Weather currentWeather =
+        await WeatherApiService().getWeather(city.latitude, city.longitude);
+
+    city.currentWeather = currentWeather;
+
+    return city;
+  }
+}
