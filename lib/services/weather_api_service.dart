9050aa40310ae1d133788b8166161528889cff97
DiegoDefSan <defsan35@gmail.com>
2023-11-13 20:28:43 -0300

    Add WeatherApiService class for fetching weather

---
+++ lib/services/weather_api_service.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:previsao_do_tempo/models/weather.dart';
+import 'package:previsao_do_tempo/utils/constants.dart';
+import 'package:previsao_do_tempo/utils/converse_tempeture.dart';
+
+class WeatherApiService {
+  final String baseUrl = "http://api.openweathermap.org/data/2.5/forecast";
+
+  Future<Weather> getWeather(double latitude, double longitude) async {
+    final response = await http.get(
+        Uri.parse("$baseUrl?lat=${latitude}&lon=${longitude}&appid=$apiKey"));
+
+    if (response.statusCode == 200) {
+      final jsonResponse = jsonDecode(response.body);
+
+      final Map<String, dynamic> weatherData = jsonResponse["list"][0];
+
+      print(weatherData["main"]["temp"]);
+
+      Weather weather = Weather(
+        date: DateTime.fromMillisecondsSinceEpoch(weatherData["dt"] * 1000),
+        currentTemp: kelvinToCelsius(weatherData["main"]["temp"]),
+        precipitationProbability: (weatherData["pop"] * 100).toDouble(),
+        humidity: weatherData["main"]["humidity"],
+        windSpeed: (weatherData["wind"]["speed"]).toDouble(),
+        description: weatherData["weather"][0]["main"],
+      );
+
+      return weather;
+    } else {
+      throw Exception("Failed to load weather");
+    }
+  }
+}
