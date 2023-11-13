3c3b3f9517e0ee1886f6412d6d36ea80c0d38eb3
DiegoDefSan <defsan35@gmail.com>
2023-11-13 08:34:32 -0300

    Add ErrorRoutePage to handle errors

---
+++ lib/pages/ErrorRoutePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ErrorRoutePage extends StatelessWidget {
+  const ErrorRoutePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Error"),
+      ),
+      body: Container(),
+    );
+  }
+}
