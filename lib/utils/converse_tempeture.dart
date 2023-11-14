cdb7aa709ee563c4c25d6cbd411d79249d65c317
DiegoDefSan <defsan35@gmail.com>
2023-11-14 19:00:19 -0300

    Add functions to convert date and get weekday

--- lib/utils/converse_tempeture.dart
+++ lib/utils/converse_tempeture.dart
@@ -1,3 +1,33 @@
 int kelvinToCelsius(double kelvin) {
   return (kelvin - 273.15).round();
 }
+
+// Function that have a string datetime and return only date
+String convertDateTime(String dateString) {
+  DateTime dateTime = DateTime.parse(dateString);
+
+  String onlyDate =
+      "${dateTime.year}-${dateTime.month.toString().padLeft(2, '0')}-${dateTime.day.toString().padLeft(2, '0')}";
+
+  return onlyDate;
+}
+
+String getWeekDay(String dateString) {
+  DateTime date = DateTime.parse(dateString);
+
+  int weekIndex = date.weekday;
+
+  List<String> weekDays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+
+  String day = weekDays[weekIndex - 1];
+
+  return day;
+}
