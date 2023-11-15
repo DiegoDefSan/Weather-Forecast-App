9837e14c600eaebb4d117bcd789bbf067e180374
DiegoDefSan <defsan35@gmail.com>
2023-11-15 08:39:44 -0300

    Update gases dictionary with correct gas names

--- lib/utils/gases_dict.dart
+++ lib/utils/gases_dict.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:previsao_do_tempo/widgets/air_quality/GasNameContainer.dart';
 
 const Map<String, Widget> containerByGasName = {
-  "SO2": GasNameContainer(gasName: "SO", subIndex: "2"),
-  "NO2": GasNameContainer(gasName: "NO", subIndex: "2"),
-  "PM10": GasNameContainer(gasName: "PM", subIndex: "10"),
-  "PM2.5": GasNameContainer(gasName: "PM", subIndex: "2.5"),
-  "O3": GasNameContainer(gasName: "O", subIndex: "3"),
   "CO": GasNameContainer(gasName: "CO", subIndex: ""),
-  "CO2": GasNameContainer(gasName: "CO", subIndex: "2"),
+  "NO": GasNameContainer(gasName: "NO", subIndex: ""),
+  "NO2": GasNameContainer(gasName: "NO", subIndex: "2"),
+  "O3": GasNameContainer(gasName: "O", subIndex: "3"),
+  "SO2": GasNameContainer(gasName: "SO", subIndex: "2"),
+  "PM2.5": GasNameContainer(gasName: "PM", subIndex: "2.5"),
+  "PM10": GasNameContainer(gasName: "PM", subIndex: "10"),
   "NH3": GasNameContainer(gasName: "NH", subIndex: "3"),
 };
